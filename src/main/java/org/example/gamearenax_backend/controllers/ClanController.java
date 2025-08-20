c1bd85bf2ba4b93086b076292468d4fa902a58af
nethushinavodya <nethushiperera@gmail.com>
2025-08-20 23:46:20 +0530

    Updated the controller with end points for clan handling part

---
+++ src/main/java/org/example/gamearenax_backend/controllers/ClanController.java
@@ -0,0 +1,65 @@
+package org.example.gamearenax_backend.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.example.gamearenax_backend.dto.ClanDTO;
+import org.example.gamearenax_backend.dto.ResponseDTO;
+import org.example.gamearenax_backend.service.ClanService;
+import org.example.gamearenax_backend.util.VarList;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/clan")
+@RequiredArgsConstructor
+public class ClanController {
+    private final ClanService clanService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDTO> createClan(@RequestBody ClanDTO clanDTO) {
+        try {
+            ClanDTO createdClan = clanService.createClan(clanDTO);
+            return ResponseEntity.ok(new ResponseDTO(VarList.OK, "Clan created successfully", createdClan));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest()
+                    .body(new ResponseDTO(VarList.Not_Acceptable, e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<ResponseDTO> getAllClans() {
+        try {
+            List<ClanDTO> clans = clanService.getAllClans();
+            return ResponseEntity.ok(new ResponseDTO(VarList.OK, "Clans retrieved successfully", clans));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest()
+                    .body(new ResponseDTO(VarList.Not_Acceptable, e.getMessage(), null));
+        }
+    }
+
+    // Get all clans that are open
+    @GetMapping("/getAllClansByOpen")
+    public ResponseEntity<ResponseDTO> getAllClansByOpen() {
+        try {
+            List<ClanDTO> clans = clanService.getAllClansByOpen();
+            return ResponseEntity.ok(new ResponseDTO(VarList.OK, "Clans retrieved successfully", clans));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest()
+                    .body(new ResponseDTO(VarList.Not_Acceptable, e.getMessage(), null));
+        }
+    }
+
+    //get all by ranking point in ascending order
+    @GetMapping("/rankingPointsAsc")
+    public ResponseEntity<ResponseDTO> getAllClansByRankingPointsAsc() {
+        try {
+            List<ClanDTO> clans = clanService.getAllClansByRankingPointsAsc();
+            return ResponseEntity.ok(new ResponseDTO(VarList.OK, "Clans retrieved successfully by ranking points ", clans));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest()
+                    .body(new ResponseDTO(VarList.Not_Acceptable, e.getMessage(), null));
+        }
+    }
+
+}
