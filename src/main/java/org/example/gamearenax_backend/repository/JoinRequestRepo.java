0112be9f804ab2a00e96f6180fe04929bb8d6109
nethushinavodya <nethushiperera@gmail.com>
2025-08-20 23:51:47 +0530

    Handled the end points where players request the clan to join the clan

---
+++ src/main/java/org/example/gamearenax_backend/repository/JoinRequestRepo.java
@@ -0,0 +1,14 @@
+package org.example.gamearenax_backend.repository;
+
+import org.example.gamearenax_backend.entity.JoinRequest;
+import org.hibernate.boot.archive.internal.JarProtocolArchiveDescriptor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface JoinRequestRepo extends JpaRepository<JoinRequest, Long> {
+    List<Object> findByClanId(UUID uuid);
+}
